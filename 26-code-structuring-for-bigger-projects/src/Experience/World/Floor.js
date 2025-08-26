0756c346bf30939bd8431860b30a11394bd14eb3
Arthur Canz <arthur.canzian@arkx.cc>
2025-08-26 18:47:41 -0300

    react todo

--- 26-code-structuring-for-bigger-projects/src/Experience/World/Floor.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Floor.js
@@ -17,7 +17,7 @@ export default class Floor
 
     setGeometry()
     {
-        this.geometry = new THREE.CircleGeometry(5, 64)
+        this.geometry = new THREE.CircleGeometry(50, 64)
     }
 
     setTextures()
